2d6ff8e084e56a1acfb859b78b1992330c0d3cf6
Zheng Hao Tan <tanzhao@umich.edu>
2014-05-09 17:30:40 +0800

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Zheng Hao Tan on 5/9/14.
+//  Copyright (c) 2014 Zheng Hao Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
