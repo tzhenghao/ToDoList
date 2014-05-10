23ceb88539c9fa3eec670dacd3301f58d878cbfb
Zheng Hao Tan <tanzhao@umich.edu>
2014-05-10 10:52:07 +0800

    Added unwind features to the modal segue

---
+++ XYZAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZAddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by Zheng Hao Tan on 5/10/14.
+//  Copyright (c) 2014 Zheng Hao Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAddToDoItemViewController : UIViewController
+
+@end
---
+++ XYZMyToDoListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  XYZMyToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Zheng Hao Tan on 5/10/14.
+//  Copyright (c) 2014 Zheng Hao Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZMyToDoListTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *) segue;
+
+@end
